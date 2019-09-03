f7535350f7ccc0f2019137420c790188626d5f67
smjmoj <smjmoj@gmail.com>
2019-09-03 13:30:24 +0100

    Removed phantom output

--- routes/output.tf
+++ routes/output.tf
@@ -1,12 +1,1 @@
-##
-output "common-nat-id-az1" {
-  value = "${module.common-nat-az1.natid}"
-}
 
-output "common-nat-id-az2" {
-  value = "${module.common-nat-az2.natid}"
-}
-
-output "common-nat-id-az3" {
-  value = "${module.common-nat-az3.natid}"
-}
