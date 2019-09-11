f255441163c9634ee25a1a4d1df1527349103ae1
smjmoj <smjmoj@gmail.com>
2019-09-11 16:29:22 +0100

    Added the peering accepter routes for private subnets.

--- bastion-vpc/peering.tf
+++ bastion-vpc/peering.tf
@@ -17,6 +17,7 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
 
 }
 
+# public
 resource "aws_route" "peer_route_az1" {
   count = "${length(var.bastion_peering_ids)}"
   route_table_id = "${module.bastion-public-az1.routetableid}"
@@ -34,6 +35,28 @@ resource "aws_route" "peer_route_az2" {
 resource "aws_route" "peer_route_az3" {
   count = "${length(var.bastion_peering_ids)}"
   route_table_id = "${module.bastion-public-az3.routetableid}"
+  vpc_peering_connection_id = "${element(split(",", var.bastion_peering_ids[count.index] ), 0)}"
+  destination_cidr_block = "${element(split(",", var.bastion_peering_ids[count.index] ), 1)}"
+}
+
+# private
+resource "aws_route" "peer_route_private_az1" {
+  count = "${length(var.bastion_peering_ids)}"
+  route_table_id = "${module.bastion-private-az1.routetableid}"
+  vpc_peering_connection_id = "${element(split(",", var.bastion_peering_ids[count.index] ), 0)}"
+  destination_cidr_block = "${element(split(",", var.bastion_peering_ids[count.index] ), 1)}"
+}
+
+resource "aws_route" "peer_route_private_az2" {
+  count = "${length(var.bastion_peering_ids)}"
+  route_table_id = "${module.bastion-private-az2.routetableid}"
+  vpc_peering_connection_id = "${element(split(",", var.bastion_peering_ids[count.index] ), 0)}"
+  destination_cidr_block = "${element(split(",", var.bastion_peering_ids[count.index] ), 1)}"
+}
+
+resource "aws_route" "peer_route_private_az3" {
+  count = "${length(var.bastion_peering_ids)}"
+  route_table_id = "${module.bastion-private-az3.routetableid}"
   vpc_peering_connection_id = "${element(split(",", var.bastion_peering_ids[count.index] ), 0)}"
   destination_cidr_block = "${element(split(",", var.bastion_peering_ids[count.index] ), 1)}"
 }
