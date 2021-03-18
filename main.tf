5198993c129aeece3d30f62b7bd6b5ba2ac7bd78
smjmoj <smjmoj@gmail.com>
2021-03-18 16:42:50 +0000

    Root directory upgraded to TF0.12

--- main.tf
+++ main.tf
@@ -1,44 +1,30 @@
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  # When creating comment out this bit, as terragrunt will try and create the s3 bucket and cause issues.
-  # After bucket is create rename terraform.tfstate: mv terraform.tfstate terraform.tfstate1
-  # Remember to comment out assume role line when running terraform
-  # Below will build for dev environment
-  # Run command: terraform plan -var-file common.tfvars -var-file dev.tfvars
-  backend "s3" {}
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 2.69.0"
-
-  # # Comment out when using terraform only
-  # assume_role {
-  #   role_arn = "${var.role_arn}"
-  # }
-}
-
 resource "aws_kms_key" "remote_state" {
   description             = "This key is used to encrypt ${var.remote_state_bucket_name} bucket"
   deletion_window_in_days = 10
-  tags                    = "${merge(var.tags, map("Name", "${var.remote_state_bucket_name}-kms-key"))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "${var.remote_state_bucket_name}-kms-key"
+    },
+  )
 }
 
 resource "aws_kms_alias" "remote_state" {
   name          = "alias/${var.remote_state_bucket_name}-kms-key"
-  target_key_id = "${aws_kms_key.remote_state.key_id}"
+  target_key_id = aws_kms_key.remote_state.key_id
 }
 
 module "remote_state" {
-  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//remote_state"
-  remote_state_bucket_name = "${var.remote_state_bucket_name}"
-  kms_key_arn              = "${aws_kms_key.remote_state.arn}"
-  tags                     = "${var.tags}"
+  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules//remote_state?ref=terraform-0.12"
+  remote_state_bucket_name = var.remote_state_bucket_name
+  kms_key_arn              = aws_kms_key.remote_state.arn
+  tags                     = var.tags
 }
 
 module "dynamodb-table" {
-  source     = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//dynamodb-tables"
+  source     = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules//dynamodb-tables?ref=terraform-0.12"
   table_name = "${var.environment_identifier}-lock-table"
-  tags       = "${var.tags}"
+  tags       = var.tags
   hash_key   = "LockID"
 }
+
