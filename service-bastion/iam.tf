8203d70dedcc00d0775998dfc67c7f3bc587715f
adeweetman-al <89138740+adeweetman-al@users.noreply.github.com>
2022-09-09 11:58:32 +0100

    Adding role and instance profile to enable SSM connections (#92)
    * Adding role and instance profile to enable SSM connections
    
    * fix to bastion ssm role

---
+++ service-bastion/iam.tf
@@ -0,0 +1,42 @@
+locals {
+  instance_role_name = "SSMBastionAccess"
+}
+
+# Derive assume permissions
+data "aws_iam_policy_document" "instance_iam_role_assume_permissions" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# Define IAM role
+resource "aws_iam_role" "instance_iam_role" {
+  name               = local.instance_role_name
+  assume_role_policy = data.aws_iam_policy_document.instance_iam_role_assume_permissions.json
+  description        = "Allows EC2 instances to call AWS services on your behalf."
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.environment_name
+    }
+  )
+}
+
+# Attach managed policy to the role
+resource "aws_iam_role_policy_attachment" "instance_iam_policy_attach_amazonssmmanagedinstancecore" {
+  role       = aws_iam_role.instance_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create the instance profile (referenced by the aws_instance)
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = local.instance_role_name
+  role = aws_iam_role.instance_iam_role.name
+}
--- service-bastion/main.tf
+++ service-bastion/main.tf
@@ -38,10 +38,11 @@ data "aws_ami" "hmpps" {
 }
 
 resource "aws_instance" "bastion_instance" {
-  ami           = data.aws_ami.hmpps.id
-  instance_type = "t2.micro"
-  key_name      = module.bastion_ssh_key.deployer_key
-  subnet_id     = data.terraform_remote_state.bastion_vpc.outputs.bastion-public-subnet-az1
+  ami                  = data.aws_ami.hmpps.id
+  instance_type        = "t2.micro"
+  key_name             = module.bastion_ssh_key.deployer_key
+  subnet_id            = data.terraform_remote_state.bastion_vpc.outputs.bastion-public-subnet-az1
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   vpc_security_group_ids = [
     data.terraform_remote_state.bastion_vpc.outputs.bastion_vpc_sg_id,
